d53cb023719329571026a32880568cf6c6fc3288
AleksandrT <ascwork86@gmail.com>
2024-11-28 15:04:46 +0500

    fix style btn

--- components/UI/TheButton.vue
+++ components/UI/TheButton.vue
@@ -35,34 +35,31 @@ export default {
     border: solid 1px transparent;
     padding: 16px 38px;
     cursor: pointer;
-    border-radius: 5px;
+    border-radius: var(--radius);
     transition: 0.3s;
     display: flex;
     align-items: center;
+    
     &:active {background-color: tomato}
     &.btn-dark {
-      background-color: var(--primary);
+      background-color: rgb(var(--primary));
       color: #fff;
+      &:hover {
+        @media(min-width: 1023px) {
+          background-color: rgb(var(--primary));
+          filter: brightness(90%);
+          color: #fff;
+        }
+      }
     }
-    // &.btn-dark {
-    //   background-color: var(--primary);
-    //   color: #2B2B2B;
-    //   &:hover {
-    //     @media(min-width: 1023px) {
-    //       //background-color: darken(#FED700, 2%);
-    //     }
-    //   }
-    // }
-    // &.btn-light {
-    //   border-color: #FED700;
-    //   color: #2B2B2B;
-    //   background-color: #fff;
-    //   &:hover {
-    //     @media(min-width: 1023px) {
-    //       //background-color: rgba($color: #FED700, $alpha: 0.2);
-    //     }
-    //   }
-    // }
+    &.btn-light {
+      border-color: rgb(var(--primary));
+      &:hover {
+        @media(min-width: 1023px) {
+          background: rgba(var(--primary),  0.2);
+        }
+      }
+    }
   }
 
 
